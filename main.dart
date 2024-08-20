b30438539a6132da23a264dee187d867b1711135
Wasif Khan <m.wasif9905@gmail.com>
2024-08-21 00:03:34 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        floatingActionButtonTheme: FloatingActionButtonThemeData(backgroundColor: Colors.green),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(backgroundColor: Colors.blue),
       ),
       debugShowCheckedModeBanner: false,
       home: MyHomePage(),
